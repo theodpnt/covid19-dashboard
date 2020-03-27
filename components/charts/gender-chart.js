f19245201337a3ab8c288811407731f7ef9d9f3d
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2020-03-27 23:03:40 +0100

    Add Counter to GenderChart

--- components/charts/gender-chart.js
+++ components/charts/gender-chart.js
@@ -2,13 +2,21 @@ import React from 'react'
 import {Doughnut} from 'react-chartjs-2'
 import colors from '../../styles/colors'
 import 'chartjs-plugin-labels' // eslint-disable-line import/no-unassigned-import
+import Counter from '../counter'
 
 const allData = [5245, 525] //  Données de test
 
 const options = {
+  responsive: true,
+  title: {
+    display: true,
+    fontSize: 20,
+    text: 'Genre'
+  },
   plugins: {
     labels: {
       fontColor: colors.white,
+      fontSize: 14,
       fontStyle: 'bold'
     }
   }
@@ -32,8 +40,23 @@ const data = {
   }]
 }
 
-const GenderChart = () => (
-  <Doughnut data={data} options={options} />
-)
+const GenderChart = () => {
+  return (
+    <div className='gender-container'>
+      <Doughnut data={data} options={options} height={200} />
+      <div className='gender-counter'>
+        <Counter value={data.datasets[0].data[0]} label={data.labels[0]} color='blue' />
+        <Counter value={data.datasets[0].data[1]} label={data.labels[1]} color='lightBlue' />
+      </div>
+      <style jsx>{`
+        .gender-counter {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-evenly;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 export default GenderChart
