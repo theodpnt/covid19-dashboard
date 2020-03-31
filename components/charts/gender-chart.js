5200df579af0a7d6a7eb186da897c1259f48242d
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2020-03-31 15:02:01 +0200

    Add onComplete animation option to GenderChart

--- components/charts/gender-chart.js
+++ components/charts/gender-chart.js
@@ -1,26 +1,10 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import {Doughnut} from 'react-chartjs-2'
 import colors from '../../styles/colors'
-import 'chartjs-plugin-labels' // eslint-disable-line import/no-unassigned-import
 import Counter from '../counter'
+import Chart from 'chart.js'
 
 const allData = [5245, 525] //  Données de test
-
-const options = {
-  responsive: true,
-  title: {
-    display: true,
-    fontSize: 20,
-    text: 'Genre'
-  },
-  plugins: {
-    labels: {
-      fontColor: colors.white,
-      fontSize: 14,
-      fontStyle: 'bold'
-    }
-  }
-}
 
 const data = {
   labels: [
@@ -41,9 +25,46 @@ const data = {
 }
 
 const GenderChart = () => {
+  const chartRef = useRef(null)
+  const options = {
+    responsive: true,
+    tooltip: {
+      enable: true
+    },
+    events: false,
+    animation: {
+      duration: 500,
+      easing: 'easeOutQuart',
+      onComplete: animation => {
+        const {ctx} = animation.chart
+        const {data} = chartRef.current.props
+        console.log(ctx.font)
+        ctx.font = Chart.helpers.fontString(15, 'bold', Chart.defaults.global.defaultFontFamily)
+        ctx.textAlign = 'center'
+        ctx.textBaseline = 'bottom'
+        ctx.context = {}
+        data.datasets.forEach(dataset => {
+          for (let i = 0; i < dataset.data.length; i++) {
+            const model = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model
+            const total = dataset._meta[Object.keys(dataset._meta)[0]].total
+            const midRadius = model.innerRadius + ((model.outerRadius - model.innerRadius) / 2)
+            const startAngle = model.startAngle
+            const endAngle = model.endAngle
+            const midAngle = startAngle + ((endAngle - startAngle) / 2)
+            const x = midRadius * Math.cos(midAngle)
+            const y = midRadius * Math.sin(midAngle)
+            ctx.fillStyle = '#fff'
+            const percent = String(Math.round(dataset.data[i] / total * 100)) + '%'
+            ctx.fillText(percent, model.x + x, model.y + y + 15)
+          }
+        })
+      }
+    }
+  }
+
   return (
     <div className='gender-container'>
-      <Doughnut data={data} options={options} height={200} />
+      <Doughnut ref={chartRef} data={data} options={options} height={200} />
       <div className='gender-counter'>
         <Counter value={data.datasets[0].data[0]} label={data.labels[0]} color='blue' />
         <Counter value={data.datasets[0].data[1]} label={data.labels[1]} color='lightBlue' />
