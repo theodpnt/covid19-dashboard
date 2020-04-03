0df34fb8cfc273343af2400150a971cea2cfce4f
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2020-04-03 16:13:08 +0200

    Updates

---
+++ _next/static/ey6jHAAyEwEqn6SFONZ7e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b]}}("static\u002Fchunks\u002F4a3ea9cd.0b7a28f954dd6d45a0ba.js","static\u002Fchunks\u002F2c796e83.a8c7cc48515f8cc30ef5.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
