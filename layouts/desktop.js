a6d52118ea11d607561f94cf53281e3430e5e837
tmerlier <theophile.merliere@gmail.com>
2020-04-07 14:20:15 +0200

    Desktop layout selector

--- layouts/desktop.js
+++ layouts/desktop.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import DateNav from '../components/date-nav'
 import Scrollable from '../components/scrollable'
@@ -7,12 +7,12 @@ import Statistics from '../components/statistics'
 import Informations from '../components/informations'
 
 import colors from '../styles/colors'
+import LayoutSelector from '../components/layout-selector'
 
-const DesktopPage = () => {
-  return (
+const LAYOUTS = {
+  'Vue d’ensemble': () => (
     <>
       <div className='menu'>
-        <DateNav />
         <Scrollable>
           <>
             <Statistics />
@@ -26,20 +26,68 @@ const DesktopPage = () => {
       </div>
 
       <style jsx>{`
-      .menu {
-        z-index: 1;
-        display: flex;
-        flex-direction: column;
-        max-width: 500px;
-        box-shadow: 0 1px 4px ${colors.lightGrey};
-      }
+        .menu {
+          z-index: 1;
+          display: flex;
+          flex-direction: column;
+          max-width: 500px;
+          box-shadow: 0 1px 4px ${colors.lightGrey};
+        }
 
-      .map {
-        flex: 1;
-        height: 100%;
-      }
-    `}</style>
+        .map {
+          flex: 1;
+          height: 100%;
+        }
+      `}</style>
     </>
+  ),
+  'Suivi des hospitalisations': () => (
+    <h1>Suivi des hospitalisations</h1>
+  ),
+  'Suivi des tests': () => (
+    <h1>
+      Suivi des tests
+    </h1>
+  )
+}
+
+const DesktopPage = () => {
+  const [selectedLayout, setSelectedLayout] = useState(Object.keys(LAYOUTS)[0])
+  console.log("DesktopPage -> selectedLayout", selectedLayout)
+
+  return (
+    <div className='desktop-container'>
+      <div className='desktop-header'>
+        <DateNav />
+        <LayoutSelector
+          selected={selectedLayout}
+          layouts={Object.keys(LAYOUTS)}
+          selectLayout={setSelectedLayout}
+        />
+      </div>
+
+      <div className='desktop-content'>
+        {LAYOUTS[selectedLayout]()}
+      </div>
+
+      <style jsx>{`
+        .desktop-container {
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+        }
+
+        .desktop-header {
+          z-index: 10;
+          display: flex;
+          flex-direction: column;
+          box-shadow: 0 1px 4px ${colors.lightGrey};
+        }
+        .desktop-content {
+          display: flex;
+        }
+    `}</style>
+    </div>
   )
 }
 
