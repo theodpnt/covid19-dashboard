732287c452c51a032eabaf7352c6f3cea0574937
tmerlier <theophile.merliere@gmail.com>
2020-04-07 14:20:15 +0200

    Desktop layout selector

--- layouts/desktop.js
+++ layouts/desktop.js
@@ -18,6 +18,7 @@ import Statistics from '../components/statistics'
 import Informations from '../components/informations'
 
 import colors from '../styles/colors'
+import LayoutSelector from '../components/layout-selector'
 
 const DesktopPage = () => {
   const {selectedLocation, selectedMapIdx, setSelectedMapIdx} = useContext(AppContext)
@@ -113,7 +114,6 @@ const DesktopPage = () => {
   return (
     <>
       <div className='menu'>
-        <DateNav />
         <Scrollable>
           <>
             <Statistics />
@@ -176,6 +176,54 @@ const DesktopPage = () => {
         }
     `}</style>
     </>
+  ),
+  'Suivi des hospitalisations': () => (
+    <h1>Suivi des hospitalisations</h1>
+  ),
+  'Suivi des tests': () => (
+    <h1>
+      Suivi des tests
+    </h1>
+  )
+}
+
+const DesktopPage = () => {
+  const [selectedLayout, setSelectedLayout] = useState(Object.keys(LAYOUTS)[0])
+  console.log("DesktopPage -> selectedLayout", selectedLayout)
+
+  return (
+    <div className='desktop-container'>
+      <div className='desktop-header'>
+        <DateNav />
+        <LayoutSelector
+          selected={selectedLayout}
+          layouts={Object.keys(LAYOUTS)}
+          selectLayout={setSelectedLayout}
+        />
+      </div>
+
+      <div className='desktop-content'>
+        {LAYOUTS[selectedLayout]()}
+      </div>
+
+      <style jsx>{`
+        .desktop-container {
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+        }
+
+        .desktop-header {
+          z-index: 10;
+          display: flex;
+          flex-direction: column;
+          box-shadow: 0 1px 4px ${colors.lightGrey};
+        }
+        .desktop-content {
+          display: flex;
+        }
+    `}</style>
+    </div>
   )
 }
 
