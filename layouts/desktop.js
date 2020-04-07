be2858df90c39dda515417b12b3ba874a68084ef
tmerlier <theophile.merliere@gmail.com>
2020-04-07 13:24:42 +0200

    Rename Screen into Desktop

--- layouts/desktop.js
+++ layouts/desktop.js
@@ -1,51 +1,94 @@
 import React, {useContext} from 'react'
 
-import {AppContext, ThemeContext} from '../pages'
-
-import colors from '../styles/colors'
+import {AppContext} from '../pages'
 
 import DateNav from '../components/date-nav'
-import LayoutSelector from '../components/layout-selector'
+import Scrollable from '../components/scrollable'
+import ReactMapGL from '../components/react-map-gl'
+import Statistics from '../components/statistics'
+import Informations from '../components/informations'
 
-const HEADER_HEIGHT = '50px'
+import theme from '../styles/theme'
+import colors from '../styles/colors'
+
+import Drom from '../components/react-map-gl/drom'
+import MapSelector from '../components/map-selector'
 
 const DesktopPage = () => {
-  const {selectedLayout} = useContext(AppContext)
-  const themeContext = useContext(ThemeContext)
+  const {selectedLocation, selectedMapIdx, setSelectedMapIdx} = useContext(AppContext)
 
   return (
-    <div className='desktop-container'>
-      <div className='desktop-header'>
+    <>
+      <div className='menu'>
         <DateNav />
-        <LayoutSelector />
+        <Scrollable>
+          <>
+            <Statistics />
+            <Informations />
+          </>
+        </Scrollable>
       </div>
 
-      <div className='desktop-content'>
-        {selectedLayout.component}
+      <div className='map'>
+        <div className='metropole'>
+          <div className='map-selector'>
+            <MapSelector mapIdx={selectedMapIdx} selectMap={setSelectedMapIdx} />
+          </div>
+          <ReactMapGL code={selectedLocation || 'FR'} />
+        </div>
+        <div className='drom-container'>
+          <Drom />
+        </div>
       </div>
 
       <style jsx>{`
-        .desktop-container {
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-        }
+      .menu {
+        z-index: 1;
+        display: flex;
+        flex-direction: column;
+        max-width: ${theme.menuWidth};
+        box-shadow: 0 1px 4px ${colors.lightGrey};
+      }
 
-        .desktop-header {
-          z-index: 10;
-          display: flex;
-          background-color: ${themeContext.primary};
-          box-shadow: 0 1px 4px ${colors.lightGrey};
-          width: 100%;
-          height: ${HEADER_HEIGHT};
-        }
+      .map {
+        display: flex;
+        flex: 1;
+        flex-direction: column;
+        height: 100%;
+      }
 
-        .desktop-content {
-          display: flex;
-          height: calc(100% - ${HEADER_HEIGHT});
+      .metropole {
+        flex: 1;
+      }
+
+      .drom-container {
+        display: flex;
+        padding: 0.5em;
+        height: 25%;
+      }
+
+      .map-selector {
+        z-index: 1;
+        position: absolute;
+        background-color: #000000aa;
+        color: #fff;
+        border-radius: 4px;
+        margin: 0.5em;
+      }
+
+      @media (max-width: 1000px) {
+        .drom-container {
+          height: 40%;
         }
+      }
+
+      @media (max-width: 800px) {
+        .drom-container {
+          height: 50%;
+        }
+      }
     `}</style>
-    </div>
+    </>
   )
 }
 
