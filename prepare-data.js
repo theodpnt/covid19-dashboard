ca6bc958c3a1929e5252e4806d498074b37d3aae
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2020-04-21 07:18:15 +0200

    Add sources to prepare-data.js

--- prepare-data.js
+++ prepare-data.js
@@ -3,14 +3,28 @@ require('dotenv').config()
 
 const {join} = require('path')
 const got = require('got')
+const Papa = require('papaparse')
 const {outputJson, readJson} = require('fs-extra')
-const {groupBy, sortBy, defaults, pick} = require('lodash')
+const {groupBy, sortBy, defaults, pick, union, flattenDeep} = require('lodash')
 
 const DATA_SOURCE = process.env.DATA_SOURCE || 'https://raw.githubusercontent.com/opencovid19-fr/data/master/dist/chiffres-cles.json'
+const DATA_GENRE = 'https://www.data.gouv.fr/fr/datasets/r/63352e38-d353-4b54-bfd1-f1b3ee1cabd7'
+const DATA_AGE = 'https://www.data.gouv.fr/fr/datasets/r/08c18e08-6780-452d-9b8c-ae244ad529b3'
+const DATA_COVID_DAY = 'https://www.data.gouv.fr/fr/datasets/r/b4ea7b4b-b7d1-4885-a099-71852291ff20'
+const DATA_COVID_WEEK = 'https://www.data.gouv.fr/fr/datasets/r/72050bc8-9959-4bb1-88a0-684ff8db5fb5'
 
 async function fetchJson(url) {
   const response = await got(url, {responseType: 'json'})
   return response.body
+}
+
+async function fetchCsv(urls) {
+  const promises = urls.map(url => got(url, {responseType: 'text'}))
+  return Promise.all(promises).then(responses => {
+    return responses.map(response => {
+      return Papa.parse(response.body, {header: true}).data
+    })
+  })
 }
 
 async function loadJson(dataSource) {
@@ -19,6 +33,74 @@ async function loadJson(dataSource) {
   }
 
   return readJson(dataSource)
+}
+
+function dataGenre(data) {
+  return flattenDeep(data).filter(r => 'sexe' in r).map(r => {
+    return {
+      code: `DEP-${r.dep}`,
+      sexe: Number.parseInt(r.sexe, 10),
+      date: r.jour,
+      hospitalises: Number.parseInt(r.hosp, 10),
+      reanimation: Number.parseInt(r.rea, 10),
+      deces: Number.parseInt(r.dc, 10),
+      gueris: Number.parseInt(r.rad, 10),
+      source: {nom: 'Santé publique France Data'},
+      sourceType: 'sante-publique-france-data'
+    }
+  })
+}
+
+function dataAge(data) {
+  return flattenDeep(data).filter(r => 'cl_age90' in r).map(r => {
+    return {
+      code: `REG-${r.reg}` || `DEP-${r.dep}`,
+      date: r.jour,
+      trancheAge: r.cl_age90,
+      hospitalises: Number.parseInt(r.hosp, 10),
+      reanimation: Number.parseInt(r.rea, 10),
+      deces: Number.parseInt(r.dc, 10),
+      gueris: Number.parseInt(r.rad, 10),
+      source: {nom: 'Santé publique France Data'},
+      sourceType: 'sante-publique-france-data'
+    }
+  })
+}
+
+function dataCovidDay(data) {
+  return flattenDeep(data).filter(r => 'clage_covid' in r && 'jour' in r).map(r => {
+    return {
+      code: `DEP-${r.dep}`,
+      date: r.jour,
+      trancheAge: r.clage_covid,
+      nbTest: Number.parseInt(r.nb_test, 10),
+      nbPositif: Number.parseInt(r.nb_pos, 10),
+      nbTestHomme: Number.parseInt(r.nb_test_h, 10),
+      nbPositifHomme: Number.parseInt(r.nb_pos_h, 10),
+      nbTestFemme: Number.parseInt(r.nb_test_f, 10),
+      nbPositifFemme: Number.parseInt(r.nb_pos_f, 10),
+      source: {nom: 'Santé publique France Data'},
+      sourceType: 'sante-publique-france-data'
+    }
+  })
+}
+
+function dataCovidWeek(data) {
+  return flattenDeep(data).filter(r => 'clage_covid' in r && 'week' in r).map(r => {
+    return {
+      code: `DEP-${r.dep}`,
+      date: r.jour,
+      trancheAge: r.clage_covid,
+      nbTest: Number.parseInt(r.nb_test, 10),
+      nbPositif: Number.parseInt(r.nb_pos, 10),
+      nbTestHomme: Number.parseInt(r.nb_test_h, 10),
+      nbPositifHomme: Number.parseInt(r.nb_pos_h, 10),
+      nbTestFemme: Number.parseInt(r.nb_test_f, 10),
+      nbPositifFemme: Number.parseInt(r.nb_pos_f, 10),
+      source: {nom: 'Santé publique France Data'},
+      sourceType: 'sante-publique-france-data'
+    }
+  })
 }
 
 const SOURCE_PRIORITIES = {
@@ -64,6 +146,9 @@ function filterRecords(records) {
 
 async function main() {
   const records = await loadJson(DATA_SOURCE)
+  const csvRecords = await fetchCsv([DATA_GENRE, DATA_AGE, DATA_COVID_DAY, DATA_COVID_WEEK])
+  const hospitalisation = union(dataGenre(csvRecords), dataAge(csvRecords))
+  const covidTests = union(dataCovidDay(csvRecords), dataCovidWeek(csvRecords))
   const data = consolidate(filterRecords(records))
   await outputJson(join(__dirname, 'chiffres-cles.json'), data, {spaces: 2})
 }
